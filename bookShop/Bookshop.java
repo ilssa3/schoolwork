0d15e7d40b74b610ed7ff32d29af579253e4d6b9
Iliyana Iskarova <116028692+ilssa3@users.noreply.github.com>
2024-01-03 12:58:54 +0200

    Update Bookshop.java

--- bookShop/Bookshop.java
+++ bookShop/Bookshop.java
@@ -8,6 +8,15 @@ public class Bookshop {
 	Scanner sc = new Scanner (System.in);
 	Book b = new Book(null, 0, 0, 0, null, null, null);
 	b.printInfo();
+System.out.println("-----");
+	Atlass a = new Atlass(null, 0, 0, 0, null, null, null, null, 0);
+	a.printInfo();
+System.out.println("-----");	
+	Journal j = new Journal(null, 0, 0, 0, null, null, 0, null);
+	j.printInfo();
+System.out.println("-----");
+	
 	}
 
 }
+
