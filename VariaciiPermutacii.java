10b424f82f81859d62e0bd9c388a49a61f75ef78
Iliyana Iskarova <116028692+ilssa3@users.noreply.github.com>
2024-04-23 19:30:24 +0300

    Update VariaciiPermutacii.java

--- VariaciiPermutacii.java
+++ VariaciiPermutacii.java
@@ -6,8 +6,12 @@ import java.util.Scanner;
 public class VariaciiPermutacii {
 
 	 public void permutation(String s) {
+		 //inputvam s za da go convertna v charArray
 	        char[] original = s.toCharArray();
 	        Arrays.sort(original);
+	        //sortiram i inicializiram v dva masiva
+	        //clone - store-va permutaciqta koqto e genrirana v momenta
+	        //mark - markira char izpolzvani veche v perm
 	        char[] clone = new char[s.length()];
 	        boolean[] mark = new boolean[s.length()];
 	        Arrays.fill(mark, false);
@@ -15,18 +19,28 @@ public class VariaciiPermutacii {
 	    }
 
 	    private void permute(char[] original, char[] clone, boolean[] mark, int length, int n) {
+	    	//rekursivniq metod generirasht permutaciqta
+	    	//original - char array za originalniq string
+	    	//clone - char array za segashen generiran string
+	    	//mark - za izpolzvani veche char
+	    	//length - za duljinata na vmomentashnata permutaciq
+	    	//n - krainata duljina na string-a
 	        if (length == n) {
+	        	//proverqva dali generirashtiq se string e s duljinata na finalniq
+	        	//ako e true => che e zavurshen; printira permutaciqta 
 	            System.out.println(clone);
 	            return;
 	        }
-
+//inache s bool mark - markira char koito obhojda za izpolzvan 
+	        
 	        for (int i = 0; i < n; i++) {
 	            if (mark[i] == true) continue;
-	            // dont use this state. to keep order of duplicate character
+	           //dobavq go v permutaciqta i generira rekursivno ot ostanalite char 
+		       //ostanalite vuzmojnosti
 	            if (i > 0 && original[i] == original[i-1] && mark[i-1] == false) continue;
 	            mark[i] = true;
 	            clone[length] = original[i];
-	            permute(original, clone, mark, length+1, n);
+	            permute(original, clone, mark, length+1, n);//recursivno 
 	            mark[i] = false;
 	        }
 
